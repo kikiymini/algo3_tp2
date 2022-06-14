9878e96b77ea8a55dd9cc3d7c71919f39db1c101
Saibot19 <106347614+Saibot19@users.noreply.github.com>
2022-06-14 11:10:53 -0300

    CAMBIE LA LOGICA DE LA GRILLA. IMPORTANTE. CHEQUEAR QUE NO HAYA ERRORES.

--- src/main/java/edu/fiuba/algo3/modelo/Grilla/Grilla.java
+++ src/main/java/edu/fiuba/algo3/modelo/Grilla/Grilla.java
@@ -1,14 +1,13 @@
 package edu.fiuba.algo3.modelo.Grilla;
 
-import edu.fiuba.algo3.modelo.Accionable;
-import edu.fiuba.algo3.modelo.Calle;
-import edu.fiuba.algo3.modelo.Constantes;
-import edu.fiuba.algo3.modelo.Obstaculo.Pozo;
-import edu.fiuba.algo3.modelo.Posicion;
-import edu.fiuba.algo3.modelo.Vehiculos.Vehiculo;
+        import edu.fiuba.algo3.modelo.Accionable;
+        import edu.fiuba.algo3.modelo.Calle;
+        import edu.fiuba.algo3.modelo.Constantes;
+        import edu.fiuba.algo3.modelo.Posicion;
+        import edu.fiuba.algo3.modelo.Vehiculos.Vehiculo;
 
-import java.util.ArrayList;
-import java.util.Random;
+        import java.util.ArrayList;
+        import java.util.Random;
 
 public class Grilla {
 
@@ -40,17 +39,12 @@ public class Grilla {
         return this.tamanioEjeX * this.tamanioEjeY;
     }
 
-
-    private int obtenerCantTotalPosicionesDeManzanas(){
-        return (this.cantEsquinasEjeX + 1) * (this.cantEsquinasEjeY + 1);
-    }
-
     private int obtenerCantTotalDeEsquinas(){
         return this.cantEsquinasEjeX * this.cantEsquinasEjeY;
     }
 
     public int obtenerCantTotalDePosValidas(){
-        return this.obtenerCantTotalDePosiciones() - this.obtenerCantTotalPosicionesDeManzanas() - this.obtenerCantTotalDeEsquinas();
+        return this.obtenerCantTotalDePosiciones() - this.obtenerCantTotalDeEsquinas();
     }
 
     public Accionable obtenerAccionableEnPosicion(Posicion pos){
@@ -64,13 +58,13 @@ public class Grilla {
     }
 
     private void asignarCantidadesDeEsquinas(){
-        this.cantEsquinasEjeX = Grilla.conseguirUnaCantEsquinasPorEje();
-        this.cantEsquinasEjeY = Grilla.conseguirUnaCantEsquinasPorEje();
+        this.cantEsquinasEjeX = this.conseguirUnaCantEsquinasPorEje();
+        this.cantEsquinasEjeY = this.cantEsquinasEjeX;
     }
 
     private void asignarTamaniosEjes(){
         this.tamanioEjeX = (this.cantEsquinasEjeX * 3) + 2;
-        this.tamanioEjeY = (this.cantEsquinasEjeY * 3) + 2;
+        this.tamanioEjeY = this.tamanioEjeX;
     }
 
 
@@ -97,3 +91,11 @@ public class Grilla {
         return grilla;
     }
 }
+
+
+
+
+
+
+
+
